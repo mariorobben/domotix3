6fa50aeeabf2f403cd891441f642ce7a37be8a0b
robben.mario@telenet.be <YY@7fM0Zz4^BSZ57cz^XI0p1>
2021-10-23 09:29:34 +0200

    windows

--- src/domotix3/domotix3.cpp
+++ src/domotix3/domotix3.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#ifdef _WIN32
+
+int main(int argc, char** argv)
+{
+	std::cout << "hello windows" << std::endl;
+	return 0;
+}
+
+#else
+
+#include <iostream>
 
 #include "kbus_adi.hpp"
 
@@ -7,34 +18,34 @@
 using namespace std;
 using namespace wago;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    adi_kbus_device wago;
+	adi_kbus_device wago;
 
-    auto status = wago.get_status();
-    cout << "status" << endl;
+	auto status = wago.get_status();
+	cout << "status" << endl;
 	cout << "get_bit_count: " << status.get_bit_count() << endl;
 	cout << "get_terminal_count: " << status.get_terminal_count() << endl;
 	cout << "get_bit_count_analog_input: " << status.get_bit_count_analog_input() << endl;
 	cout << "get_bit_count_analog_output: " << status.get_bit_count_analog_output() << endl;
 	cout << "get_bit_count_digital_input: " << status.get_bit_count_digital_input() << endl;
 	cout << "get_bit_count_digital_output: " << status.get_bit_count_digital_output() << endl;
-    cout << endl;
+	cout << endl;
 
-    auto terminals = wago.get_terminals();
-    cout << "terminals" << endl;
-    for (auto terminal : terminals)
-    {
-	    cout << "get_position: " << terminal.get_position() << endl;
-	    cout << "get_type: " << terminal.get_type() << endl;
-	    cout << "get_bit_offset_out: " << terminal.get_bit_offset_out() << endl;
-	    cout << "get_bit_size_out: " << terminal.get_bit_size_out() << endl;
-	    cout << "get_bit_offset_in: " << terminal.get_bit_offset_in() << endl;
-	    cout << "get_bit_size_in: " << terminal.get_bit_size_in() << endl;
-	    cout << "get_channels: " << terminal.get_channels() << endl;
-	    cout << "get_pi_format: " << terminal.get_pi_format() << endl;
-        cout << endl;
-    }
+	auto terminals = wago.get_terminals();
+	cout << "terminals" << endl;
+	for (auto terminal : terminals)
+	{
+		cout << "get_position: " << terminal.get_position() << endl;
+		cout << "get_type: " << terminal.get_type() << endl;
+		cout << "get_bit_offset_out: " << terminal.get_bit_offset_out() << endl;
+		cout << "get_bit_size_out: " << terminal.get_bit_size_out() << endl;
+		cout << "get_bit_offset_in: " << terminal.get_bit_offset_in() << endl;
+		cout << "get_bit_size_in: " << terminal.get_bit_size_in() << endl;
+		cout << "get_channels: " << terminal.get_channels() << endl;
+		cout << "get_pi_format: " << terminal.get_pi_format() << endl;
+		cout << endl;
+	}
 
 	while (true)
 	{
@@ -46,5 +57,7 @@ int main(int argc, char **argv)
 		cout << "." << flush;
 	}
 
-    return 0;
+	return 0;
 }
+
+#endif
